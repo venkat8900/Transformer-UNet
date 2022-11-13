0fadde2ddd7de34735a87710eb2489a1b5158b74
Venkat Mukthineni <venkatmukthineni2000@gmail.com>
2022-11-13 18:16:56 -0500

    fixed prepare RTS lists train code

--- prepare_RTS_lists.py
+++ prepare_RTS_lists.py
@@ -1,6 +1,9 @@
 import os
 
-def prep_list(self, data_dir, save_dir, split):
+# TODO
+# modify code for test set
+
+def prep_list(data_dir, save_dir, split):
     '''
     args:
     - data_dir: path to directory where images and corresponding masks are present
@@ -8,16 +11,18 @@ def prep_list(self, data_dir, save_dir, split):
     - split: train or test
     '''
     # read all the image names in every dataset
+    names_list = []
     for i in range(1, 9):
-        names_list = []
-        data_dir_i = data_dir + 'instrument_dataset_' + i + '/images/'
+        
+        data_dir_i = data_dir + split + '/' + 'instrument_dataset_' + str(i) + '/images/'
+        print(data_dir_i)
         for image in os.listdir(data_dir_i):
             names_list.append(data_dir_i + image)
         
-        with open(save_dir + split, 'w') as fp:
-            for n in names_list:
-                fp.write("%s\n" % n)
-            print("[INFO] Added instrument_dataset_{} {} into {}", i, split, save_dir)
+    with open(save_dir + split, 'w') as fp:
+        for n in names_list:
+            fp.write("%s\n" % n)
+        print("[INFO] Added instrument_dataset_{} {} into {}", format(i), format(split), format(save_dir))
 
 
 
@@ -25,8 +30,6 @@ def prep_list(self, data_dir, save_dir, split):
 if __name__ == "__main__":
 
     # create save directory and list text files
-    if not os.path.exists("./lists/lists_RTS_binary"):
-        os.makedirs("./lists/lists_RTS_binary")
     if not os.path.exists("./lists/lists_RTS_binary/train.txt"):
         f = open("./lists/lists_RTS_binary/train.txt", "w")
         print("[INFO] train text file created for binary RTS")
@@ -38,8 +41,6 @@ if __name__ == "__main__":
     else:
         print("[INFO] test text file exists for binary RTS")
 
-    if not os.path.exists("./lists/lists_RTS_instrument"):
-        os.makedirs("./lists/lists_RTS_instrument")
     if not os.path.exists("./lists/lists_RTS_instrument/train.txt"):
         f = open("./lists/lists_RTS_instrument/train.txt", "w")
         print("[INFO] train text file created for instrument RTS")
@@ -51,8 +52,6 @@ if __name__ == "__main__":
     else:
         print("[INFO] test text file exists for instrument RTS")
     
-    if not os.path.exists("./lists/lists_RTS_parts"):
-        os.makedirs("./lists/lists_RTS_parts")
     if not os.path.exists("./lists/lists_RTS_parts/train.txt"):
         f = open("./lists/lists_RTS_parts/train.txt", "w")
         print("[INFO] train text file created for parts RTS")
@@ -71,6 +70,6 @@ if __name__ == "__main__":
         data_dir = './data/RTS_' + t + '/'
         save_dir = './lists/lists_RTS_' + t + '/'
 
-        prep_list(data_dir, save_dir, "train")
-        prep_list(data_dir, save_dir, "test") 
+        prep_list(data_dir = data_dir, save_dir = save_dir, split = "train")
+        # prep_list(data_dir = data_dir, save_dir = save_dir, split = "test") 
     
