36166efba50b657fb7b89cb778895e4b3eebf59d
Venkat Mukthineni <venkatmukthineni2000@gmail.com>
2022-11-13 19:02:35 -0500

    RTS dataloader

--- datasets/dataset_RTS.py
+++ datasets/dataset_RTS.py
@@ -1,50 +1,30 @@
-# TODO
-'''
-1) prepare dataloader
-2) refer: https://github.com/venkat8900/Robotic-Tool-Segmentation
-'''
-
 import os
-import random
-import h5py
-import numpy as np
 import torch
 from torch.utils.data import Dataset
+import cv2
+from pathlib import Path
 
 
 class RTS_dataset(Dataset):
-    def __init__(self, base_dir, list_dir, split, transform=None):
+    def __init__(self, base_dir, list_dir, split, task, transform=None):
         self.transform = transform  # using transform in torch!
         self.split = split
         self.sample_list = open(os.path.join(list_dir, self.split+'.txt')).readlines()
         self.data_dir = base_dir
+        self.task = task
 
     def __len__(self):
         return len(self.sample_list)
 
     def __getitem__(self, idx):
-        if self.split == "train":
-            slice_name = self.sample_list[idx].strip('\n')
-
-            #TODO
-            # load the data and labels
-            # Think opencv dataloading might work
-            '''
-            data_path = os.path.join(self.data_dir, slice_name+'.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
-            '''
-        else:
-            vol_name = self.sample_list[idx].strip('\n')
-
-            #TODO
-            # load the data and labels
-            # Think opencv dataloading might work
-            '''
-            filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
-            data = h5py.File(filepath)
-            image, label = data['image'][:], data['label'][:]
-            '''
+        task_dict = {'binary': 'binary_masks', 'instrument': 'instruments_masks', 'parts': 'parts_masks'}
+        
+        slice_path = self.sample_list[idx].strip('\n')
+        base_path = Path(slice_path).resolve().parents[1]
+        img_name = os.path.basename(slice_path)
+        label_path = os.path.join(base_path, task_dict[self.task], img_name)
+        image = cv2.imread(slice_path)
+        label = cv2.imread(label_path)
 
         sample = {'image': image, 'label': label}
         if self.transform:
--- rough.py
+++ rough.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
 
-if not os.path.exists("./lists/lists_RTS_binary/train.txt"):
-    f = open("./lists/lists_RTS_binary/train.txt", "w")
-    print("[INFO] train text file created for binary RTS")
-else:
-    print("[INFO] train text file exists for binary RTS")
+p = './data/RTS_binary/train/instrument_dataset_1/images/frame000.jpg'
+d = Path(p).resolve().parents[1]
+
+c = os.path.basename(p)
+print(c)
+print(d)
+task = 'binary_masks'
+e = os.path.join(d, task, c)
+
+print(e)
